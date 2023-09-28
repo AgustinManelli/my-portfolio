e06aabd8b358a4b24c41db2ba320ff9a500b76f0
Agustin Manelli <agustinmanelli@hotmail.com>
2023-09-28 20:43:01 -0300

    add image component

---
+++ src/components/ImageClipPath.js
@@ -0,0 +1,33 @@
+import "../stylesheets/ImageClipPath.css";
+
+function ImageClipPath(props) {
+  return (
+    <div className="ImageClipPathContainer">
+      <svg width="0" height="0">
+        <defs>
+          <clipPath id="squircle" clipPathUnits="objectBoundingBox">
+            <path d="M .5,0 C .1,0 0,.1 0,.5 0,.9 .1,1 .5,1 .9,1 1,.9 1,.5 1,.1 .9,0 .5,0 Z"></path>
+          </clipPath>
+        </defs>
+      </svg>
+      <div
+        className="ImageClipPath"
+        style={{
+          backgroundImage: `url(${props.src})`,
+          height: `${props.h}`,
+          width: `${props.w}`,
+        }}
+      ></div>
+      <div
+        className="ImageClipPathBlurred"
+        style={{
+          backgroundImage: `url(${props.src})`,
+          height: `${props.h}`,
+          width: `${props.w}`,
+        }}
+      ></div>
+    </div>
+  );
+}
+
+export default ImageClipPath;
--- src/components/home/HomeHeader.js
+++ src/components/home/HomeHeader.js
@@ -1,20 +1,21 @@
 import "../../stylesheets/HomeHeader.css";
 import Button from "../Button";
-import { AiFillGithub, AiFillLinkedin } from "react-icons/ai";
+import { AiFillGithub, AiFillLinkedin, AiOutlineFile } from "react-icons/ai";
+import ImageClipPath from "../ImageClipPath";
 
 function HomeHeader() {
   return (
     <div style={{ overflow: "hidden", width: "100vw" }}>
-      <div class="wrapper">
-        <div class="gradient gradient-1"></div>
-        <div class="gradient gradient-2"></div>
-        <div class="gradient gradient-3"></div>
+      <div className="wrapper">
+        <div className="gradient gradient-1"></div>
+        <div className="gradient gradient-2"></div>
+        <div className="gradient gradient-3"></div>
       </div>
       <div className="homeHeaderPattern"></div>
       <div className="homeHeaderContainer">
         <section className="homeHeaderSections">
           <h1>Agustin Manelli</h1>
-          <p>front-end developer</p>
+          <p>frontend developer.</p>
           <div className="homeHeader_buttonsContainer">
             <Button
               icon={<AiFillGithub />}
@@ -26,10 +27,21 @@ function HomeHeader() {
               name="linkedin"
               link="https://www.linkedin.com/in/agustinmanelli/"
             />
+            <Button
+              icon={<AiOutlineFile />}
+              name="Curriculum Vitae"
+              link="https://www.linkedin.com/in/agustinmanelli/"
+            />
           </div>
         </section>
         <section className="homeHeaderSections">
           <div className="homeHeaderSubSections">
+            <ImageClipPath
+              src="https://media.tycsports.com/files/2023/06/24/584696/messi_862x485.webp?v=1"
+              alr="image"
+              w="250px"
+              h="250px"
+            />
             <div>
               <p>lorem ipsum</p>
               <p>lorem ipsum</p>
