86f0447bad5f3898306dfdcad05eb8fdc6f98526
Agustin Manelli <agustinmanelli@hotmail.com>
2023-10-01 08:27:00 -0300

    update project card

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -5,9 +5,15 @@ function Projects() {
     <section className="ProjectsContainer">
       <header>THIS IS THE TITLE OF PROJECTS SECTION</header>
       <div className="Projects">
-        <ProjectsItems year="2023" />
-        <ProjectsItems />
-        <ProjectsItems />
+        <ProjectsItems
+          year="2023"
+          date="jun 20, 2023"
+          title="bepixel"
+          desc="Deserunt incididunt officia irure amet aliquip dolore amet ad
+            laboris laborum aliqua do est nulla. Qui voluptate ea consequat
+            nostrud ex laboris laboris consequat aliqua sunt nisi mollit."
+          tags={["hola", "si", "react"]}
+        />
       </div>
     </section>
   );
--- src/components/projects/ProjectsItems.js
+++ src/components/projects/ProjectsItems.js
@@ -1,12 +1,34 @@
 import "./ProjectsItems.css";
+import logo from "../../assets/ISOTIPO.svg";
+import Button from "../Button";
+import { AiFillGithub } from "react-icons/ai";
+import { useEffect } from "react";
 
 function ProjectsItems(props) {
+  const tags = props.tags;
+  console.log(tags);
   return (
     <div className="ProjectItemsContainer">
-      <p>{props.year}</p>
+      <p className="ProjectItemsDate">{props.year}</p>
       <section className="ProjectItemsContentContainer">
-        <div className="ProjectItemsBlurred"></div>
-        <div className="ProjectItemsContent"></div>
+        <div className="ProjectItemsContent">
+          <div className="ProjectItemsBlurred"></div>
+          <p className="ProjectItemsContent_date">{props.date}</p>
+          <img src={logo} className="ProjectItemsContent_logo" />
+          <h2 className="ProjectItemsContent_title">{props.title}</h2>
+          <p className="ProjectItemsContent_desc">{props.desc}</p>
+          <footer>
+            <section className="ProjectItemsContent_tags">
+              {tags.map((tag) => (
+                <p>{tag}</p>
+              ))}
+            </section>
+            <section className="ProjectItemsContent_buttons">
+              <a>live</a>
+              <Button icon={<AiFillGithub />} />
+            </section>
+          </footer>
+        </div>
       </section>
     </div>
   );
