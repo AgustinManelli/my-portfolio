71e1ff99ec697f309148f5f9de458f1a6460abad
Agustin Manelli <agustinmanelli@hotmail.com>
2023-10-01 10:54:03 -0300

    update project card styles

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,22 +1,37 @@
 import "./Projects.css";
 import ProjectsItems from "./ProjectsItems";
 import logo from "../../assets/ISOTIPO.svg";
+import project1 from "../../assets/project1.jpg";
+import impuestarlogo from "../../assets/impuestarlogo.jpg";
 function Projects() {
   return (
     <section className="ProjectsContainer">
-      <header>THIS IS THE TITLE OF PROJECTS SECTION</header>
+      <header>.</header>
       <div className="Projects">
         <ProjectsItems
           logo={logo}
+          preview={project1}
           year="2023"
-          date="jun 20, 2023"
+          date="mar 3, 2023"
           title="bepixel"
-          desc="Deserunt incididunt officia irure amet aliquip dolore amet ad
-            laboris laborum aliqua do est nulla. Qui voluptate ea consequat
-            nostrud ex laboris laboris consequat aliqua sunt nisi mollit."
+          desc="Página web para agencia de diseño gráfico con buscado de herramientas"
           tags={["React", "Supabase", "FramerMotion", "i18next"]}
+          visit={true}
           link="https://bepixel.vercel.app/"
           repo="https://github.com/AgustinManelli/bepixel"
+          color="#bc03ff"
+        />
+        <ProjectsItems
+          logo={impuestarlogo}
+          year="2022"
+          date="may 1, 2022"
+          title="Impuestar keylogger"
+          desc="Keylogger encapsulado en aplicación de cálculo de impuestos."
+          tags={["Python", "Tkinter"]}
+          visit={false}
+          link="https://bepixel.vercel.app/"
+          repo="https://github.com/AgustinManelli/bepixel"
+          color="#0055ff"
         />
       </div>
     </section>
--- src/components/projects/ProjectsItems.js
+++ src/components/projects/ProjectsItems.js
@@ -1,18 +1,66 @@
 import "./ProjectsItems.css";
 import Button from "../Button";
-import { AiFillGithub } from "react-icons/ai";
+import {
+  AiFillGithub,
+  AiOutlineFullscreen,
+  AiOutlineFullscreenExit,
+} from "react-icons/ai";
 import { ImArrowUpRight2 } from "react-icons/im";
+import { useState } from "react";
 
 function ProjectsItems(props) {
+  const [openPreview, setOpenPreview] = useState(true);
   const tags = props.tags;
+
+  const togglePreview = () => {
+    setOpenPreview(!openPreview);
+  };
+
   return (
     <div className="ProjectItemsContainer">
       <p className="ProjectItemsDate">{props.year}</p>
       <section className="ProjectItemsContentContainer">
-        <div className="ProjectItemsContent">
-          <div className="ProjectItemsBlurred"></div>
+        <button
+          className="ProjectItemsContent_button_fs"
+          onClick={togglePreview}
+        >
+          {openPreview ? <AiOutlineFullscreenExit /> : <AiOutlineFullscreen />}
+        </button>
+        <div
+          className="ProjectItemsBlurred"
+          style={{ backgroundColor: `${props.color}` }}
+        ></div>
+        <img
+          src={props.preview}
+          style={
+            openPreview
+              ? {
+                  position: "absolute",
+                  width: "90%",
+                  opacity: "1",
+                  transition: "opacity .3s ease-in-out",
+                  borderRadius: "10px",
+                }
+              : {
+                  position: "absolute",
+                  width: "90%",
+                  opacity: "0",
+                  transition: "opacity .3s ease-in-out",
+                  borderRadius: "10px",
+                }
+          }
+          alt="preview project"
+        />
+        <div
+          className="ProjectItemsContent"
+          style={openPreview ? { opacity: "0" } : { opacity: "1" }}
+        >
           <p className="ProjectItemsContent_date">{props.date}</p>
-          <img src={props.logo} className="ProjectItemsContent_logo" />
+          <img
+            src={props.logo}
+            className="ProjectItemsContent_logo"
+            alt="project logo"
+          />
           <h2 className="ProjectItemsContent_title">{props.title}</h2>
           <p className="ProjectItemsContent_desc">{props.desc}</p>
           <footer>
@@ -22,17 +70,21 @@ function ProjectsItems(props) {
               ))}
             </section>
             <section className="ProjectItemsContent_buttons">
-              <a
-                className="ProjectItemsContent_goto"
-                href={props.link}
-                rel="noreferrer"
-                target="_blank"
-              >
-                <p>Visit</p>
-                <ImArrowUpRight2
-                  style={{ width: "13px", fontWeight: "bold" }}
-                />
-              </a>
+              {props.visit ? (
+                <a
+                  className="ProjectItemsContent_goto"
+                  href={props.link}
+                  rel="noreferrer"
+                  target="_blank"
+                >
+                  <p>Visit</p>
+                  <ImArrowUpRight2
+                    style={{ width: "13px", fontWeight: "bold" }}
+                  />
+                </a>
+              ) : (
+                ""
+              )}
               <Button icon={<AiFillGithub />} link={props.repo} />
             </section>
           </footer>
