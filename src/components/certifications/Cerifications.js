4242dc3dbb2cd19ccda29538a985263fb63ed278
Agustin Manelli <agustinmanelli@hotmail.com>
2023-10-05 21:11:59 -0300

    update certifications

--- src/components/certifications/Cerifications.js
+++ src/components/certifications/Cerifications.js
@@ -1,4 +1,7 @@
 import CertificationsItems from "./CertificationsItems";
+import { BiLogoReact } from "react-icons/bi";
+import utn from "../../assets/utn.svg";
+import google from "../../assets/activategoogle.png";
 
 function Certifications() {
   return (
@@ -11,9 +14,35 @@ function Certifications() {
           display: "flex",
           alignItems: "center",
           justifyContent: "center",
+          gap: "50px",
         }}
       >
-        <CertificationsItems color="#bc03ff" />
+        <CertificationsItems
+          color="#bc03ff"
+          thumbnail={utn}
+          techstack={<BiLogoReact className="certificationTech" />}
+          idbool={false}
+          time="nov 2022"
+          title="Testing de software"
+        />
+        <CertificationsItems
+          color="#bc03ff"
+          thumbnail={google}
+          techstack={<BiLogoReact className="certificationTech" />}
+          idbool={true}
+          idcertification="X9C LYS 34A"
+          time="apr 2022"
+          title="Desarrollo web I"
+        />
+        <CertificationsItems
+          color="#bc03ff"
+          thumbnail={google}
+          techstack={<BiLogoReact className="certificationTech" />}
+          idbool={true}
+          idcertification="X9C LYS 34A"
+          time="apr 2022"
+          title="Desarrollo web II"
+        />
       </div>
     </section>
   );
--- src/components/certifications/CertificationsItems.js
+++ src/components/certifications/CertificationsItems.js
@@ -1,19 +1,31 @@
 import "./CertificationsItems.css";
 function CertificationsItems(props) {
   return (
-    <div className="CertificationsItemsContainer">
-      <div
-        className="CertificationsItemsCircle"
-        style={{
-          backgroundColor: `${props.color}`,
-        }}
-      ></div>
-      <div className="CertificationsItems">
-        <p>time/day</p>
-        <img src={props.thumbnail} alt="certification logo" />
-        <img src={props.techstack} alt="certification tech" />
+    <a
+      href="https://uve.frc.utn.edu.ar/mod/customcert/verify_certificate.php?contextid=24526&code=cMoGvqV4ks"
+      target="_blank"
+      rel="noreferrer"
+    >
+      <div className="CertificationsItemsContainer">
+        <div
+          className="CertificationsItemsCircle"
+          style={{
+            backgroundColor: `${props.color}`,
+          }}
+        ></div>
+        <div className="CertificationsItems">
+          <p className="CertificationsItemsTime">{props.time}</p>
+          <div className="CertificationsItemsIMGTXT">
+            <img src={props.thumbnail} alt="certification logo" />
+            <p className="CertificationsItemsTitle">{props.title}</p>
+          </div>
+          {props.idbool ? (
+            <p className="CertificationsItemsId">id: {props.idcertification}</p>
+          ) : undefined}
+          {props.techstack}
+        </div>
       </div>
-    </div>
+    </a>
   );
 }
 
