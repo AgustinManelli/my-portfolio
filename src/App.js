205c73903cf2b86d148435e3c7184248ed6f4066
Agustin Manelli <agustinmanelli@hotmail.com>
2023-12-25 23:25:10 -0300

    add smoothscroll with lenis and fix responsive css

--- src/App.js
+++ src/App.js
@@ -3,8 +3,20 @@ import HomeHeader from "./components/home/HomeHeader";
 import Projects from "./components/projects/Projects";
 import Certifications from "./components/certifications/Cerifications";
 import Footer from "./components/footer/Footer";
+import Lenis from "@studio-freight/lenis";
 
 function App() {
+  const lenis = new Lenis({
+    smoothWheel: true,
+    duration: 1.2,
+  });
+
+  function raf(time) {
+    lenis.raf(time);
+    requestAnimationFrame(raf);
+  }
+
+  requestAnimationFrame(raf);
   return (
     <>
       <HomeHeader />
