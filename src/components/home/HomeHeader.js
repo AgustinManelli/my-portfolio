f6626177b99aa4dcf48e64908d87a91c4484290b
Agustin Manelli <agustinmanelli@hotmail.com>
2024-08-19 18:31:21 -0300

    optimize image

--- src/components/home/HomeHeader.js
+++ src/components/home/HomeHeader.js
@@ -3,6 +3,7 @@ import Button from "../Button";
 import { AiFillGithub, AiFillLinkedin } from "react-icons/ai";
 import ImageClipPath from "../ImageClipPath";
 import myphoto from "../../assets/271193082_426444742538952_146421189548635760_n.jpeg";
+import me from "../../assets/me.webp";
 import EducationTimeline from "./EducationTimeline";
 import FloatingSection from "./FloatingSection.js";
 
@@ -36,7 +37,7 @@ function HomeHeader() {
         </section>
         <section className="homeHeaderSections">
           <div className="homeHeaderSubSections">
-            <ImageClipPath src={myphoto} alt="image" w="150px" h="150px" />
+            <ImageClipPath src={me} alt="image" w="150px" h="150px" />
             <p className="subSectionDescription">
               I have a strong foundation in React, JS, HTML, and CSS, and
               proficient in graphic design tools like Photoshop and Illustrator.
